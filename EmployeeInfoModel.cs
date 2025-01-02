3624940d168da025ca240c6d01b1a03e21ba6ad2
Simon Eke <simon3eke@gmail.com>
2025-01-02 16:30:34 +0100

    Continued working on the next issue, the EmployeeInfo point

---
+++ EmployeeInfoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbProject_School
+{
+    internal class EmployeeInfoModel
+    {
+        public int EmployeeAmount { get; set; }
+        public string EmployeeWorkRole { get; set; }
+
+        // To ensure a plural form of the workRole when printed.
+        public string FormattedWorkRole => EmployeeAmount <= 1 ? $"{EmployeeWorkRole}" : $"{EmployeeWorkRole}s";
+    }
+}
