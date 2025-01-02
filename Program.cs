552b5e7db25305985cdd3b69c1da7468fac3f4ed
Simon Eke <simon3eke@gmail.com>
2025-01-02 22:03:28 +0100

    Added the StudentInfo related classes. Started writing in the Readme. Made TODO-list in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,15 @@ namespace DbProject_School
 {
     internal class Program
     {
+
+        // TODO - Change the folder and classes within CustomModels to DTO - Data Transfer Object ?
+        // TODO - Menu
+        // TODO - Hide the connection string lol
+        // TODO - Finish up the StudentInfo classes
+        // TODO - Try to make the formatting into a separate method
+        // TODO - Write somethings in the ReadMe file
+        // TODO - Add the SQL-queries to the GitHub repo
+
         static void Main(string[] args)
         {
             // ---------------- Visa information om alla elever --------------------------
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DbProject_School
+
+ - TODO - Change the folder and classes within CustomModels to DTO - Data Transfer Object ?
+ - TODO - Menu
+ - TODO - Hide the connection string lol
+ - TODO - Finish up the StudentInfo classes
+ - TODO - Try to make the formatting into a separate method
+ - TODO - Write somethings in the ReadMe file
+ - TODO - Add the SQL-queries to the GitHub repo
