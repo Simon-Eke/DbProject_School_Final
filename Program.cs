a77d07133bb7d189ff62f1a4237c246960ceeab4
Simon Eke <simon3eke@gmail.com>
2025-01-03 14:48:58 +0100

    Updated my TODO-list in program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,13 @@ namespace DbProject_School
         // TODO - Change the folder and classes within CustomModels to DTO - Data Transfer Object ?
         // TODO - Menu
         // TODO - Hide the connection string lol
-        // TODO - Finish up the StudentInfo classes WIP
+        /*
+           TODO - Finish up the StudentInfo classes WIP
+                - Model DONE
+                - Service DONE
+                - Controller
+                - Formatter WIP
+         */
         // TODO - Try to make the formatting into a separate method
         // TODO - Write somethings in the ReadMe file
         // TODO - Add the SQL-queries to the GitHub repo
